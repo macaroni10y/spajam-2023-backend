263ea9e27651eb582a237ae640944b60c15e1962
kitajima.yuto <kitajima.yuto@furyu.jp>
2023-07-23 13:18:18 +0900

    wip attempt to create ecs, ecr, dynamodb with cdk

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,16 +1,46 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import {ApplicationLoadBalancedFargateService} from "aws-cdk-lib/aws-ecs-patterns";
+import {ContainerImage, LogDriver} from "aws-cdk-lib/aws-ecs";
+import {Repository} from "aws-cdk-lib/aws-ecr";
+import {AttributeType, Table} from "aws-cdk-lib/aws-dynamodb";
+import {LogGroup} from "aws-cdk-lib/aws-logs";
+
 
 export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const repository = new Repository(this, 'Repository', {
+            repositoryName: 'spajam-2023-api',
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const fargateService = new ApplicationLoadBalancedFargateService(this, 'FargateService', {
+            taskImageOptions: {
+                image: ContainerImage.fromEcrRepository(repository, ),
+                logDriver: LogDriver.awsLogs({
+                    logGroup: new LogGroup(this, 'AiIllustrationEcsLogGroup', {
+                        logGroupName: `/aws/ecs/logs/spajam-2023-api`,
+                    }),
+                    streamPrefix: `ecs`,
+                }),
+                containerPort: 8080,
+            },
+        });
+        fargateService.targetGroup.configureHealthCheck({
+            path: '/health',
+            port: '8080',
+        });
+
+        const dynamoDbTable = new Table(this, 'AiIllustrationDynamoDb', {
+            tableName: 'Conversation',
+            partitionKey: {
+                name: 'sealId',
+                type: AttributeType.STRING,
+            },
+        });
+
+        dynamoDbTable.grantFullAccess(fargateService.taskDefinition.taskRole);
+
+    }
 }
